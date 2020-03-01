f4b0b31e53785acb7f463084fe5ab07dfc7d7708
klesomik <klesomiks@gmail.com>
2020-03-01 23:06:19 +0300

    It doesn't work

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 obj-m += phonebook.o
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+load:
+	sudo insmod phonebook.ko
+unload:
+	sudo rmmod phonebook
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- phonebook.c
+++ phonebook.c
@@ -1,6 +1,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/module.h>
@@ -9,79 +10,105 @@
 
 MODULE_LICENSE("GPL");
 
+#define DEV_NAME "phonebook"
+
 static int major_num = 0;
 static int device_open_count = 0;
 
 static char *msg_ptr;
 
-static ssize_t device_read(struct file* flip, char* buffer, size_t len, loff_t* offset)
+static ssize_t dev_read(struct file* flip, char* buffer, size_t len, loff_t* offset)
 {
-	int bytes_read = 0;
-	if (*msg_ptr == 0)
-	{
-		return 0;
-	}
-	while (len && *msg_ptr)
-	{
-		put_user(*(msg_ptr++), buffer++);
-		len--;
-		bytes_read++;
-	}
-	return bytes_read;
+    printk(KERN_INFO "phonebook: [dev_read]: _start\n");
+
+    int bytes_read = 0;
+    while (len && *msg_ptr)
+    {
+        put_user(*(msg_ptr++), buffer++);
+        len--;
+        bytes_read++;
+    }
+
+    printk(KERN_INFO "phonebook: [dev_read]: read %d bytes\n", bytes_read);
+
+    printk(KERN_INFO "phonebook: [dev_read]: _finish");
+
+    return bytes_read;
 }
 
-static ssize_t device_write(struct file* flip, const char* buffer, size_t len, loff_t* offset)
+static ssize_t dev_write(struct file* flip, const char* buffer, size_t len, loff_t* offset)
 {
-        printk(KERN_INFO "phonebook: write not realized\n");
+    printk(KERN_INFO "phonebook: [dev_write]: _start\n");
+
+    printk(KERN_INFO "phonebook: [dev_write]: _finish\n");
+
+    return 0;
 }
 
-static int device_open(struct inode* inode, struct file* file)
+static int dev_open(struct inode* inode, struct file* file)
 {
-	if (device_open_count)
-	{
-		return -EBUSY;
-	}
-	device_open_count++;
-	try_module_get(THIS_MODULE);
-        printk(KERN_INFO "phonebook: device was opened\n");
-	return 0;
+    printk(KERN_INFO "phonebook: [dev_open]: _start\n");
+
+    if (device_open_count)
+    {
+        printk(KERN_INFO "phonebook: [dev_open]: could not open device\n");
+        return -EBUSY;
+    }
+
+    device_open_count++;
+    try_module_get(THIS_MODULE);
+    printk(KERN_INFO "phonebook: [dev_open]: device was opened\n");
+
+    printk(KERN_INFO "phonebook: [dev_open]: _finish\n");
+
+    return 0;
 }
 
-static int device_release(struct inode* inode, struct file* file)
+static int dev_release(struct inode* inode, struct file* file)
 {
-	device_open_count--;
-	module_put(THIS_MODULE);
-        printk(KERN_INFO "phonebook: device was closed\n");
-	return 0;
+    printk(KERN_INFO "phonebook: [dev_release]: _start\n");
+
+    device_open_count--;
+    module_put(THIS_MODULE);
+    printk(KERN_INFO "phonebook: [dev_release]: device was closed\n");
+
+    printk(KERN_INFO "phonebook: [dev_release]: _finish\n");
+
+    return 0;
 }
 
 struct file_operations fops = {
-        .read    = device_read,
-        .write   = device_write,
-        .open    = device_open,
-        .release = device_release
+        .read    = dev_read,
+        .write   = dev_write,
+        .open    = dev_open,
+        .release = dev_release
 };
 
 static int __init dev_init(void)
 {
-	major_num = register_chrdev(0, "phonebook", &fops);
-	if (major_num < 0)
-	{
-		printk(KERN_ALERT "phonebook: Could not register device: %d\n", major_num);
-		return major_num;
-	}
-	else
-	{
-		printk(KERN_INFO "phonebook: device major number %d\n", major_num);
-		return 0;
-	}
+    printk(KERN_INFO "phonebook: [dev_init]: _start\n");
+
+    major_num = register_chrdev(0, DEV_NAME, &fops);
+    if (major_num < 0)
+    {
+        printk(KERN_ALERT "phonebook: [dev_init]: could not register device: %d\n", major_num);
+        return major_num;
+    }
+
+    printk(KERN_INFO "phonebook: [dev_init]: device major number %d\n", major_num);
+
+    printk(KERN_INFO "phonebook: [dev_init]: _finish\n");
+
+    return 0;
 }
 
 static void __exit dev_exit(void)
 {
-	printk(KERN_INFO "phonebook: exit\n");
+    printk(KERN_INFO "phonebook: [dev_exit]: _start\n");
 
-	unregister_chrdev(major_num, "phonebook");
+    unregister_chrdev(major_num, DEV_NAME);
+
+    printk(KERN_INFO "phonebook: [dev_exit]: _finish\n");
 }
 
 module_init(dev_init);
